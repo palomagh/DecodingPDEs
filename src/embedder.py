f2ed99b6898bdb382461fa6108cbb842f61a9793
Paloma <palomagherreros@hotmail.com>
2025-11-11 16:28:06 +0100

    Add new models to embedder

--- src/embedder.py
+++ src/embedder.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision.transforms as transforms
 from timeit import default_timer
 from functools import partial
@@ -10,7 +9,6 @@ from otdd.pytorch.distance import DatasetDistance, FeatureCost
 
 from task_configs import get_data, get_optimizer_scheduler
 from utils import conv_init, embedder_init, embedder_placeholder, adaptive_pooler, to_2tuple, set_grad_state, create_position_ids_from_inputs_embeds, l2, MMD_loss
-import copy
 
 
 def otdd(feats, ys=None, src_train_dataset=None, exact=True):
@@ -31,83 +29,99 @@ def otdd(feats, ys=None, src_train_dataset=None, exact=True):
     d = dist.distance(maxsamples = len(src_train_dataset))
     return d
 
-
-class wrapper2D(torch.nn.Module):
-    def __init__(self, input_shape, output_shape, use_embedder=True, weight='base', train_epoch=0, activation=None, target_seq_len=None, drop_out=None, from_scratch=False):
-        super().__init__()
-        self.classification = (not isinstance(output_shape, tuple)) and (output_shape != 1)
-        self.output_raw = True
-
-        if weight == 'tiny':
-            arch_name = "microsoft/swin-tiny-patch4-window7-224"
-            embed_dim = 96
-            output_dim = 768
-            img_size = 224
-        elif weight == 'base':
-            arch_name = "microsoft/swin-base-patch4-window7-224-in22k"
-            embed_dim = 128
-            output_dim = 1024
-            img_size = 224
-            patch_size = 4
-
-        if self.classification:
-            modelclass = SwinForImageClassification
-        else:
-            modelclass = SwinForMaskedImageModeling
-            
-        self.model = modelclass.from_pretrained(arch_name)
-        self.model.config.image_size = img_size
-        if drop_out is not None:
-            self.model.config.hidden_dropout_prob = drop_out 
-            self.model.config.attention_probs_dropout_prob = drop_out
-
-        self.model = modelclass.from_pretrained(arch_name, config=self.model.config) if not from_scratch else modelclass(self.model.config)
-
-        if self.classification:
-            self.model.pooler = nn.AdaptiveAvgPool1d(1)
-            self.model.classifier = nn.Identity()
-            self.predictor = nn.Linear(in_features=output_dim, out_features=output_shape)
-        else:
-            self.pool_seq_dim = adaptive_pooler(output_shape[1] if isinstance(output_shape, tuple) else 1)
-            self.pool = nn.AdaptiveAvgPool2d(input_shape[-2:])
-            self.predictor = nn.Sequential(self.pool_seq_dim, self.pool)
-
-        set_grad_state(self.model, False)
-        set_grad_state(self.predictor, False)
-
-        if use_embedder:
-            self.embedder = Embeddings2D(input_shape, patch_size=patch_size, config=self.model.config, embed_dim=embed_dim, img_size=img_size)
-            embedder_init(self.model.swin.embeddings, self.embedder, train_embedder=train_epoch > 0)
-            set_grad_state(self.embedder, True)
-            self.model.swin.embeddings = self.embedder  
-
-
-    def forward(self, x):
-        if self.output_raw:
-            return self.model.swin.embeddings(x)[0]
-            
-        x = self.model(x).logits
-
-        return self.predictor(x)
-
-
 class wrapper1D(torch.nn.Module):
-    def __init__(self, input_shape, output_shape, use_embedder=True, weight='roberta', train_epoch=0, activation=None, target_seq_len=512, drop_out=None, from_scratch=False):
+    def __init__(self, input_shape, output_shape, use_embedder=True, weight='roberta', train_epoch=0, activation=None, target_seq_len=512, drop_out=None, from_scratch=False, random=False, double=False):
         super().__init__()
 
         self.dense = False
         self.output_raw = True
         self.weight = weight
         self.output_shape = output_shape
+        self.double = double
 
         if isinstance(output_shape, tuple):
             self.dense = True
 
-        if weight =='swin':
-            self.model = SwinForImageClassification.from_pretrained("microsoft/swin-base-patch4-window7-224-in22k") if not from_scratch else SwinForImageClassification()
-            self.model.pooler = nn.AdaptiveAvgPool1d(1)
-            self.model.classifier = nn.Identity() 
-
+        if weight == 'gpt2':
+            modelname = 'gpt2'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
+        elif weight == 'gpt2-medium':
+            modelname = 'gpt2-medium'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
+        elif weight == 'gpt2-large':
+            modelname = 'gpt2-large'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
+        elif weight == 'gpt2-xl':
+            modelname = 'gpt2-xl'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
+        elif weight == 'pythia-14m':
+            modelname = 'EleutherAI/pythia-14m'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
+        elif weight == 'pythia-70m':
+            modelname = 'EleutherAI/pythia-70m'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
+        elif weight == 'pythia-160m':
+            modelname = 'EleutherAI/pythia-160m'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+            
+        elif weight == 'pythia-410m':
+            modelname = 'EleutherAI/pythia-410m'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
+        elif weight == 'pythia-1b':
+            modelname = 'EleutherAI/pythia-1b'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
+        elif weight == 'pythia-1,4b':
+            modelname = 'EleutherAI/pythia-1.4b'
+            configuration = AutoConfig.from_pretrained(modelname)
+            if drop_out is not None:
+                configuration.hidden_dropout_prob = drop_out
+                configuration.attention_probs_dropout_prob = drop_out
+            self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
+        
         else:
             modelname = 'roberta-base' if weight[:7] == 'roberta' else 'bert-base-uncased'
             configuration = AutoConfig.from_pretrained(modelname)
@@ -116,10 +130,41 @@ class wrapper1D(torch.nn.Module):
                 configuration.attention_probs_dropout_prob = drop_out
             self.model = AutoModel.from_pretrained(modelname, config = configuration) if not from_scratch else AutoModel.from_config(configuration)
 
+        if random: 
+            for module_ in self.model.named_modules():
+                if isinstance(module_[1],(torch.nn.Linear, torch.nn.Embedding)):
+                    module_[1].weight.data.normal_(mean=0.0, std=self.model.config.init_std)
+                elif isinstance(module_[1], torch.nn.LayerNorm):
+                    module_[1].bias.data.zero_()
+                    module_[1].weight.data.fill_(1.0)
+                if isinstance(module_[1], torch.nn.Linear) and module_[1].bias is not None:
+                    module_[1].bias.data.zero_()
         if use_embedder:
-            self.embedder = Embeddings1D(input_shape, config=self.model.config, embed_dim=128 if weight == 'swin' else 768, target_seq_len=1024 if weight == 'swin' else target_seq_len, dense=self.dense)
-            embedder_init(self.model.swin.embeddings if weight == 'swin' else self.model.embeddings, self.embedder, train_embedder=train_epoch > 0)
-            set_grad_state(self.embedder, True)    
+            if weight == 'gpt2-medium' or weight == 'gpt2-medium' or weight == 'pythia-410m':
+                self.embedder = Embeddings1D(input_shape, embed_dim=1024, target_seq_len=target_seq_len, config=self.model.config, dense=self.dense)
+                embedder_init(self.model, self.embedder, train_embedder=train_epoch > 0)
+            elif weight == 'gpt2-large':
+                self.embedder = Embeddings1D(input_shape, embed_dim=1280, target_seq_len=target_seq_len, config=self.model.config, dense=self.dense)
+                embedder_init(self.model, self.embedder, train_embedder=train_epoch > 0)
+            elif weight == 'gpt2-xl':
+                self.embedder = Embeddings1D(input_shape, embed_dim=1600, target_seq_len=target_seq_len, config=self.model.config, dense=self.dense)
+                embedder_init(self.model, self.embedder, train_embedder=train_epoch > 0)
+            elif weight == 'pythia-14m':
+                self.embedder = Embeddings1D(input_shape, embed_dim=128, target_seq_len=target_seq_len, config=self.model.config, dense=self.dense)
+                embedder_init(self.model, self.embedder, train_embedder=train_epoch > 0)
+            elif weight == 'pythia-70m':
+                self.embedder = Embeddings1D(input_shape, embed_dim=512, target_seq_len=target_seq_len, config=self.model.config, dense=self.dense)
+                embedder_init(self.model, self.embedder, train_embedder=train_epoch > 0)
+            elif weight == 'pythia-1b' or weight == 'pythia-1,4b':
+                self.embedder = Embeddings1D(input_shape, embed_dim=2048, target_seq_len=target_seq_len, config=self.model.config, dense=self.dense)
+                embedder_init(self.model, self.embedder, train_embedder=train_epoch > 0)
+            elif weight == 'gpt2' or weight == 'pythia-160m':
+                self.embedder = Embeddings1D(input_shape, embed_dim=768, target_seq_len=target_seq_len, config=self.model.config, dense=self.dense)
+                embedder_init(self.model, self.embedder, train_embedder=train_epoch > 0)
+            else:
+                self.embedder = Embeddings1D(input_shape, config=self.model.config, embed_dim= 768, target_seq_len=target_seq_len, dense=self.dense)
+                embedder_init(self.model.embeddings, self.embedder, train_embedder=train_epoch > 0)
+                set_grad_state(self.embedder, True)    
         else:
             self.embedder = nn.Identity()
 
@@ -169,45 +214,6 @@ class wrapper1D(torch.nn.Module):
             x = x.squeeze(1)
 
         return x
-
-
-class Embeddings2D(nn.Module):
-
-    def __init__(self, input_shape, patch_size=4, embed_dim=96, img_size=224, config=None):
-        super().__init__()
-
-        self.resize, self.input_dimensions = transforms.Resize((img_size, img_size)), (img_size, img_size)
-        self.patch_size = to_2tuple(patch_size)
-        self.patched_dimensions = (self.input_dimensions[0] // self.patch_size[0], self.input_dimensions[1] // self.patch_size[1])
-        ks = self.patch_size
-        self.projection = nn.Conv2d(input_shape[1], embed_dim, kernel_size=ks, stride=self.patch_size, padding=(ks[0]-self.patch_size[0]) // 2)
-        self.norm = nn.LayerNorm(embed_dim)
-        num_patches = (self.input_dimensions[1] // self.patch_size[1]) * (self.input_dimensions[0] // self.patch_size[0])
-        
-        conv_init(self.projection)
-
-        
-    def maybe_pad(self, x, height, width):
-        if width % self.patch_size[1] != 0:
-            pad_values = (0, self.patch_size[1] - width % self.patch_size[1])
-            x = nn.functional.pad(x, pad_values)
-        if height % self.patch_size[0] != 0:
-            pad_values = (0, 0, 0, self.patch_size[0] - height % self.patch_size[0])
-            x = nn.functional.pad(x, pad_values)
-        return x
-
-
-    def forward(self, x, *args, **kwargs):
-        x = self.resize(x)
-        _, _, height, width = x.shape
-
-        x = self.maybe_pad(x, height, width)
-        x = self.projection(x)
-        x = x.flatten(2).transpose(1, 2)
-        
-        x = self.norm(x)   
-        
-        return x, self.patched_dimensions
 
 
 class Embeddings1D(nn.Module):
@@ -263,32 +269,9 @@ class Embeddings1D(nn.Module):
 def get_tgt_model(args, root, sample_shape, num_classes, loss, add_loss=False, use_determined=False, context=None, opid=0):
     
     src_train_loader, _, _, _, _, _, _ = get_data(root, args.embedder_dataset, args.batch_size, False, maxsize=5000)
-    if len(sample_shape) == 4:
-        IMG_SIZE = 224 if args.weight == 'tiny' or args.weight == 'base' else 196
-            
-        src_model = wrapper2D(sample_shape, num_classes, use_embedder=False, weight=args.weight, train_epoch=args.embedder_epochs, activation=args.activation, drop_out=args.drop_out)
-        src_model = src_model.to(args.device).eval()
-            
-        src_feats = []
-        src_ys = []
-        for i, data in enumerate(src_train_loader):
-            x_, y_ = data 
-            x_ = x_.to(args.device)
-            x_ = transforms.Resize((IMG_SIZE, IMG_SIZE))(x_)
-            out = src_model(x_)
-            if len(out.shape) > 2:
-                out = out.mean(1)
 
-            src_ys.append(y_.detach().cpu())
-            src_feats.append(out.detach().cpu())
-        src_feats = torch.cat(src_feats, 0)
-        src_ys = torch.cat(src_ys, 0).long()
-        src_train_dataset = torch.utils.data.TensorDataset(src_feats, src_ys)        
-        del src_model    
-
-    else:
-        src_feats, src_ys = src_train_loader.dataset.tensors[0].mean(1), src_train_loader.dataset.tensors[1]
-        src_train_dataset = torch.utils.data.TensorDataset(src_feats, src_ys)
+    src_feats, src_ys = src_train_loader.dataset.tensors[0].mean(1), src_train_loader.dataset.tensors[1]
+    src_train_dataset = torch.utils.data.TensorDataset(src_feats, src_ys)
         
     tgt_train_loader, _, _, n_train, _, _, data_kwargs = get_data(root, args.dataset, args.batch_size, False, get_shape=True)
     transform = data_kwargs['transform'] if data_kwargs is not None and 'transform' in data_kwargs else None
@@ -302,7 +285,7 @@ def get_tgt_model(args, root, sample_shape, num_classes, loss, add_loss=False, u
 
     tgt_train_loaders, tgt_class_weights = load_by_class(tgt_train_loader, num_classes_new)
 
-    wrapper_func = wrapper1D if len(sample_shape) == 3 else wrapper2D
+    wrapper_func = wrapper1D
     tgt_model = wrapper_func(sample_shape, num_classes, weight=args.weight, train_epoch=args.embedder_epochs, activation=args.activation, target_seq_len=args.target_seq_len, drop_out=args.drop_out)
     tgt_model = tgt_model.to(args.device).train()
 
